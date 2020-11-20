0e8929e12c156e546bd6924fd46e98b81c73dbac
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-21 00:46:26 +0530

    Update project settings

--- django_quiz/urls.py
+++ django_quiz/urls.py
@@ -1,7 +1,7 @@
-"""online_test URL Configuration
+"""django_quiz URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quiz.urls'))
+    path('', include('quiz.urls')),
 ]
--- django_quiz/wsgi.py
+++ django_quiz/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for online_test project.
+WSGI config for django_quiz project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_test.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_quiz.settings')
 
 application = get_wsgi_application()
