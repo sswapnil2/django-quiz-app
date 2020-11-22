f62413824e5c3261afb74bed5588d2d99cc832cf
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-22 15:13:46 +0530

    Update

--- django_quiz/settings.py
+++ django_quiz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j1h$vvasjpqpv&pg-bo5sz=o+#^fa)41_2z6xtzel8n^5qeckt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '*']
 
