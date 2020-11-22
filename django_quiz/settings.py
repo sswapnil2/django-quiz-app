2887d44fea409fc218fb3c12e8383bd85b76a200
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-22 15:12:29 +0530

    Update

--- django_quiz/settings.py
+++ django_quiz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'j1h$vvasjpqpv&pg-bo5sz=o+#^fa)41_2z6xtzel8n^5qeckt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '*']
 
 
 # Application definition
